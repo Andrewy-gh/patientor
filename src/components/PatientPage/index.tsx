275d94890da95818e706221dceef41b2c6a0812b
Andy-git985 <andy.yu617@gmail.com>
2023-12-29 22:46:19 -0500

    add one patient page

---
+++ src/components/PatientPage/index.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import patientService from '../../services/patients';
+import { Patient } from '../../types';
+
+const PatientPage = () => {
+  const { id } = useParams();
+  const [patient, setPatient] = useState<Patient | null>();
+
+  useEffect(() => {
+    const fetchOnePatient = async (id: string | undefined) => {
+      const patient = await patientService.getOnePatient(id);
+      setPatient(patient);
+    };
+    void fetchOnePatient(id);
+  }, [id]);
+
+  if (!patient) return <p>Invalid Patient Id</p>;
+  return (
+    <section>
+      <h1>{patient.name}</h1>
+      <div>gender: {patient.gender}</div>
+      <div>ssn: {patient.ssn}</div>
+      <div>occupation {patient.occupation}</div>
+    </section>
+  );
+};
+
+export default PatientPage;
